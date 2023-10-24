bdb4fe7a5b7a73488c1a4a662f009546fce5904c
Arthur <arthurbogacki7@gmail.com>
2023-10-24 20:55:53 -0300

    Inicio Front-End

---
+++ cadastro.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+        <head>
+            <title>Cadastro</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta charset="utf-8">
+            <link href="estilo/style.css" rel="stylesheet">
+        </head>
+    <body>
+        <div class="conteiner-cadastro">
+            <div class="space">
+            <form method="post">
+                <div class="livro">
+                    <h2 class="title1">Livro</h2>
+                    <input type="text" name="livro" placeholder="Nome do Livro">
+                    <h2 class="title2">Nota</h2>
+                    <div class="rating">
+                    <input type="radio" name="star" id="star1" value="1" class="star-radio">
+                    <label for="star1" class="star-label">&#9733;</label>
+                    <input type="radio" name="star" id="star2" value="2" class="star-radio">
+                    <label for="star2" class="star-label">&#9733;</label>
+                    <input type="radio" name="star" id="star3" value="3" class="star-radio">
+                    <label for="star3" class="star-label">&#9733;</label>
+                    <input type="radio" name="star" id="star4" value="4" class="star-radio">
+                    <label for="star4" class="star-label">&#9733;</label>
+                    <input type="radio" name="star" id="star5" value="5" class="star-radio">
+                    <label for="star5" class="star-label">&#9733;</label>
+                    </div><!--rating-->
+                </div><!--livro-->
+
+                <div class="resenha">
+                    <h2>Resenha</h2>
+                    <textarea name="resenha"></textarea>
+                    <input type="submit" value="Enviar" name="acao">
+                </div><!--resenha-->
+            </form>
+            <div class="clear"></div>
+            </div><!--space-->
+        </div><!--conteiner-->
+    </body>
+
+
+    <script>
+        const starRadios = document.querySelectorAll(".star-radio");
+        const starLabels = document.querySelectorAll(".star-label");
+
+        starRadios.forEach((radio, index) => {
+            radio.addEventListener("click", () => {
+                // Captura a classificação selecionada
+                const rating = parseInt(radio.value);
+
+                // Remove a cor amarela de todas as estrelas
+                starLabels.forEach((label) => {
+                    label.style.color = "#ddd"; // Cor das estrelas não selecionadas
+                });
+
+                // Aplica estilos apenas às estrelas selecionadas e anteriores
+                for (let i = 0; i <= index; i++) {
+                    starLabels[i].style.color = "#f0d800"; // Cor das estrelas selecionadas
+                }
+            });
+        });
+    </script>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+            <title>Biblioteca</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta charset="utf-8">
+            <meta name="keywords" content="biblioteca">
+            <meta name="description content="A Rede Social dos Leitores">
+            <link href="estilo/style.css" rel="stylesheet">
+        </head>
+
+        <body>  
+            <div class="conteiner">
+                <img src="imagens/img-principal.png">
+                <h2>BIBLIOTECA</h2>
+                <p>A Rede Social dos Leitores</p>
+            </div><!---conteiner-->
+            <div class="btn">
+                <div class="btn-single">
+                    <h2><a>Ver Livros</a></h2>
+                </div>
+                <div class="btn-single">
+                    <h2><a href="cadastro.php">Cadastrar Livros</a></h2>
+                </div>
+                
+            </div>
+        </body>
+</html>
